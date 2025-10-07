108ec2a869a0159f08ba34148d5de1b1f0739558
Ahmed Khaled <11410120240027@stud.cu.edu.eg>
2025-10-07 13:25:15 +0200

    Updated main.cpp: added new filter option

--- main.cpp
+++ main.cpp
@@ -11,6 +11,50 @@
 
 
 using namespace std;
+void cropImage(Image &image) {
+    int x, y, nWidth, nHeight;
+
+    cout << "Enter starting x (left): ";
+    cin >> x;
+    cout << "Enter starting y (top): ";
+    cin >> y;
+    cout << "Enter new width: ";
+    cin >> nWidth;
+    cout << "Enter new height: ";
+    cin >> nHeight;
+
+    if (x < 0 || y < 0 || x + nWidth > image.width || y + nHeight > image.height) {
+        cout << "Error: Crop dimensions exceed image boundaries!" << endl;
+        return;
+    }
+
+    Image cropped(nWidth, nHeight);
+
+    for (int j = 0; j < nHeight; ++j) {
+        for (int i = 0; i < nWidth; ++i) {
+            for (int k = 0; k < 3; ++k) { 
+                cropped(i, j, k) = image(x + i, y + j, k);
+            }
+        }
+    }
+
+    cout << "Cropping done successfully!\n";
+
+    cout << "If you want to save, enter 1 else 0: ";
+    int save;
+    cin >> save;
+
+    if (save == 1) {
+        string filename;
+        cout << "Enter the name of the new image with its extension: ";
+        cin >> filename;
+        cropped.saveImage(filename);
+        cout << "Image saved successfully!\n";
+    } else {
+        cout << "Image not saved.\n";
+    }
+}
+
 void blurImage(Image &image) {
    
     Image blurred(image.width, image.height);
@@ -489,7 +533,9 @@ int main() {
       cout << "6. Merge image \n";
       cout << "7. Add frame \n";
       cout << "8. Blur image \n";
-      cout << "9. Exit\n";
+      cout << "9. Crop Images \n";
+
+      cout << "12. Exit\n";
       cout << "Your choice: ";
       cin >> choice;
       switch (choice) {
@@ -642,8 +688,24 @@ int main() {
                 }
                 break;
               }
+                      case 9:
+            {
+                string filename;
+                cout << "Enter image filename: ";
+                cin >> filename;
+
+                Image img(filename);
+                if (img.imageData) {
+                    cropImage(img);
+                } else {
+                    cout << "Failed to load image.\n";
+                }
+                break;
+                                return 0;
+
+            }
                 
-        case 9:
+        case 12:
             cout << "Exiting program.\n";
             return 0;
             
